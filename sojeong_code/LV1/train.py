e433a2abbcf3982a47403d00d40e20855c1894be
Soojeoong <soojeoong43@gmail.com>
2024-09-20 13:52:30 +0300

    valid split 0.05

--- sojeong_code/LV1/train.py
+++ sojeong_code/LV1/train.py
@@ -59,7 +59,7 @@ def main(config):
         train_info = pd.read_csv(config['data_info_file'])
         
         # 데이터셋을 train과 valid로 나눔
-        train_df, val_df = train_test_split(train_info, test_size=0.1, stratify=train_info['target'])
+        train_df, val_df = train_test_split(train_info, test_size=0.05, stratify=train_info['target'])
 
         # 변환 설정 (albumentations 사용)
         transform_selector = TransformSelector(transform_type="albumentations")
