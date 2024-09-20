70cf4da3365b0c4715309e81a375beaa077263b9
Soojeoong <soojeoong43@gmail.com>
2024-09-20 16:42:18 +0300

    modify inference.py

--- sojeong_code/LV1/inference.py
+++ sojeong_code/LV1/inference.py
@@ -70,7 +70,8 @@ def main(config):
     ensure_dir_exists(output_dir)
 
     # 예측 결과 저장
-    test_info['predictions'] = predictions
+    test_info['target'] = predictions
+    test_info = test_info.reset_index().rename(columns={"index": "ID"})
     test_info.to_csv(config['output_path'], index=False)
     print(f"Predictions saved to {config['output_path']}")
 
