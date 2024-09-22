cfca0b328c9e79ba24b49f1505d29b6562e45a45
YU JEONG SEON <yjs000616@gmail.com>
2024-09-23 00:37:43 +0900

    feat-transform,normalize

--- baseline_model/datasets/transform.py
+++ baseline_model/datasets/transform.py
@@ -8,7 +8,8 @@ class AlbumentationsTransform:
     def __init__(self, is_train: bool = True):
         common_transforms = [
             A.Resize(224, 224),
-            A.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+            #A.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+            A.Normalize(mean=[0.861, 0.861, 0.861], std=[0.229, 0.224, 0.225]),
             ToTensorV2()
         ]
         if is_train:
