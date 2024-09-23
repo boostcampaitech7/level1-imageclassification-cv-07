3c58301c9d3746be91f0e6de20af4ec0a8463e76
Jihwan <ksw5464980@naver.com>
2024-09-24 00:35:26 +0900

    Feature : Cross_validation

---
+++ Model/09_24_cross_validation/inference.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+python /data/ephemeral/home/Jihwan/level1-imageclassification-cv-07/Model/09_23_vgg/inference.py --config /data/ephemeral/home/Sojeong/level1-imageclassification-cv-07/LV1/config.json
+
+# chmod +x /data/ephemeral/home/Sojeong/level1-imageclassification-cv-07/LV1/inference.sh
+# /data/ephemeral/home/Sojeong/level1-imageclassification-cv-07/LV1/inference.sh
