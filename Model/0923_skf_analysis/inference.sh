bacd5ff353c60a2ef5f6a9e7461ab872a4ebc99b
Jungyeon Seo <seocindy2002@gmail.com>
2024-09-23 17:23:30 +0900

    Feat : add baseline code

---
+++ Model/0923_skf_analysis/inference.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+python /data/ephemeral/home/Seungcheol/level1-imageclassification-cv-07/LV1/inference.py --config /data/ephemeral/home/Seungcheol/level1-imageclassification-cv-07/LV1/config.json
+
+# chmod +x /data/ephemeral/home/Seungcheol/level1-imageclassification-cv-07/LV1/inference.sh
+# /data/ephemeral/home/Seungcheol/level1-imageclassification-cv-07/LV1/inference.sh
