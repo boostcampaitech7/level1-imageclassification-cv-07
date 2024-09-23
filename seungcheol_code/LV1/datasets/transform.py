0ae5dac40bab4c3870d5761e62396aa8263619d3
sng-tory <sin12345jk77@naver.com>
2024-09-23 10:38:12 +0300

    feat : baseline

--- seungcheol_code/LV1/datasets/transform.py
+++ seungcheol_code/LV1/datasets/transform.py
@@ -7,9 +7,9 @@ from torchvision import transforms
 class AlbumentationsTransform:
     def __init__(self, is_train: bool = True):
         common_transforms = [
-            A.Resize(299, 299),
+            A.Resize(224, 224),
             A.Normalize(mean=[0.865, 0.865, 0.865], std=[0.26, 0.26, 0.26]),
-            A.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+            #A.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
             ToTensorV2()
         ]
         if is_train:
--- seungcheol_code/LV1/train.py
+++ seungcheol_code/LV1/train.py
@@ -80,7 +80,7 @@ def main(config):
         model.to(device)
 
         # 옵티마이저 및 스케줄러[]
-        optimizer = optim.SGD(model.parameters(), lr=config['learning_rate'])
+        optimizer = optim.Adam(model.parameters(), lr=config['learning_rate'])
         scheduler = StepLR(optimizer, step_size=2 * len(train_loader), gamma=0.5)
         loss_fn = nn.CrossEntropyLoss(label_smoothing=0.08)
 
