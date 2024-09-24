92587fd98e8b48c745bbc215fd0c1337ed2f7d41
Jihwan <ksw5464980@naver.com>
2024-09-24 22:08:07 +0900

    Chore

--- Model/09_24_resizing/inference.sh
+++ Model/09_24_resizing/inference.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-python /data/ephemeral/home/Jihwan/level1-imageclassification-cv-07/Model/09_23_vgg/inference.py --config /data/ephemeral/home/Sojeong/level1-imageclassification-cv-07/LV1/config.json
+python /data/ephemeral/home/Jihwan/level1-imageclassification-cv-07/Model/09_24_resizing/inference.py --config /data/ephemeral/home/Jihwan/level1-imageclassification-cv-07/Model/09_24_resizing/inference.sh
 
-# chmod +x /data/ephemeral/home/Sojeong/level1-imageclassification-cv-07/LV1/inference.sh
-# /data/ephemeral/home/Sojeong/level1-imageclassification-cv-07/LV1/inference.sh
+# chmod +x /data/ephemeral/home/Jihwan/level1-imageclassification-cv-07/Model/09_24_resizing/inference.sh
+# /data/ephemeral/home/Jihwan/level1-imageclassification-cv-07/Model/09_24_resizing/inference.sh
