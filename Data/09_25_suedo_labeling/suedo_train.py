a1abc3ce2034074808b58b238a0065bfd061f91f
Jihwan <ksw5464980@naver.com>
2024-09-25 21:42:14 +0900

    Feat : 가짜 라벨링

---
+++ Data/09_25_suedo_labeling/suedo_train.py
@@ -0,0 +1,21 @@
+import os
+import json
+import torch
+import pandas as pd
+from datasets.custom_dataset import CustomDataset
+from datasets.transform import TransformSelector
+from models.model_selector import ModelSelector
+from torch.utils.data import DataLoader
+from tqdm import tqdm
+import argparse
+
+def inference(model, device, test_loader):
+    model.eval()
+    predictions = []
+    with torch.no_grad():
+        for images in tqdm(test_loader, desc="Inference"):
+            images = images.to(device)
+            outputs = model(images)
+            preds = torch.argmax(outputs, dim=1)
+            predictions.extend(preds.cpu().numpy())
+    return predictions
