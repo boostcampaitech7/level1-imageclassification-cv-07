b3d89703c31fc930fc7cdb7fefda7a695a9adf35
sng-tory <sin12345jk77@naver.com>
2024-09-27 04:58:25 +0300

    feat : final

--- Data/Jungyeon/0922_dataAug_analysis/inference.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-python /data/ephemeral/home/Jungyeon/level1-imageclassification-cv-07/Data/0922_data_analysis/inference.py --config /data/ephemeral/home/Jungyeon/level1-imageclassification-cv-07/Data/0922_data_analysis/config.json
-
-# chmod +x /data/ephemeral/home/Jungyeon/level1-imageclassification-cv-07/Data/0922_data_analysis/inference.sh
-# /data/ephemeral/home/Jungyeon/level1-imageclassification-cv-07/Data/0922_data_analysis/inference.sh
--- Model/Jungyeon/0923_skf_analysis/inference.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-python /data/ephemeral/home/Jungyeon/level1-imageclassification-cv-07/Model/Jungyeon/0923_skf_analysis/inference.py --config /data/ephemeral/home/Jungyeon/level1-imageclassification-cv-07/Model/Jungyeon/0923_skf_analysis/config.json
-
-# chmod +x /data/ephemeral/home/Jungyeon/level1-imageclassification-cv-07/Model/Jungyeon/0923_skf_analysis/inference.sh
-# /data/ephemeral/home/Jungyeon/level1-imageclassification-cv-07/Model/Jungyeon/0923_skf_analysis/inference.sh
