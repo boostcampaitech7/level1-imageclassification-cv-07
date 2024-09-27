d9f1d248ae6469c02f71d3fadbde0003687690cd
sng-tory <sin12345jk77@naver.com>
2024-09-27 04:34:12 +0300

    feat : ensemble,hyperparameter

---
+++ Model/Seungcheol/0925_vit/inference.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+python /data/ephemeral/home/Seungcheol/level1-imageclassification-cv-07/seungcheol_code/LV1/inference.py --config /data/ephemeral/home/Seungcheol/level1-imageclassification-cv-07/seungcheol_code/LV1/config.json
+
+# chmod +x /data/ephemeral/home/Seungcheol/level1-imageclassification-cv-07/seungcheol_code/LV1/inference.sh
+# /data/ephemeral/home/Seungcheol/level1-imageclassification-cv-07/seungcheol_code/LV1/inference.sh
